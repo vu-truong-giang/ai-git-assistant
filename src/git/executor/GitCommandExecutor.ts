cc0cc0cad2c24eff44ed0c56b43ff140b4fc7e4f
Vu Truong Giang <vutruonggiangty@gmail.com>
2026-09-26 22:49:07 +0700

    tạo gitcommadExecutor

--- src/git/executor/GitCommandExecutor.ts
+++ src/git/executor/GitCommandExecutor.ts
@@ -1,33 +1,19 @@
-import * as cp from 'child_process';
+import { exec } from "child_process";
+import { promisify } from "util";
 
-export class GitCommandExecutor {
-    constructor(private workspaceRoot: string) {}
+const execAsync = promisify(exec);
 
-    async execute(args: string[]): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const process = cp.spawn('git', args, { cwd: this.workspaceRoot });
-            let stdout = '';
-            let stderr = '';
+export class GitCommandExecutor { 
+    async execute(
+        args: string[],
+        cwd: string
+    ): Promise<string> {
+        const command = `git ${args.join('')}`;
 
-            process.stdout.on('data', (data: Buffer) => {
-                stdout += data.toString();
-            });
-
-            process.stderr.on('data', (data: Buffer) => {
-                stderr += data.toString();
-            });
-
-            process.on('close', (code: number | null) => {
-                if (code === 0) {
-                    resolve(stdout);
-                } else {
-                    reject(new Error(`Git command failed with exit code ${code}: ${stderr}`));
-                }
-            });
-
-            process.on('error', (err: Error) => {
-                reject(new Error(`Failed to spawn git command: ${err.message}`));
-            });
-        });
+        const {stdout , stderr} = await execAsync( command , { cwd });
+        if (stderr) {
+            console.warn(`Git warining: ${stderr}`);
+        } 
+        return stdout;
     }
 }
